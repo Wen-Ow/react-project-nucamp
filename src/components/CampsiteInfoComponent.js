56fc8be8f55d6810b8146fd8780273673fb24390
Wen Ow <ow.wen2022@gmail.com>
2021-10-23 10:28:33 -0400

    Use React Redux Form Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,74 +1,158 @@
-import React from "react";
-import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from "reactstrap";
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,
+    Form, FormGroup, Input, Label, Row, Col
+} from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Link, Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
-function RenderCampsite({campsite}) {
-    return (
-      <div className="col-md-5 m-1">
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      </div>
+class CommentForm extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+      isModalOpen: false
+  };
+  this.toggleModal = this.toggleModal.bind(this);
+}
+
+toggleModal() {
+  this.setState({
+      isModalOpen: !this.state.isModalOpen
+  });
+}
+
+handleSubmit(values) {
+  console.log('Current state is: ' + JSON.stringify(values));
+  alert('Current state is: ' + JSON.stringify(values));
+  this.toggleModal();
+}
+
+render() {
+    return (<>
+        <Button color="primary" className="fa fa-pencil fa-lg" outline onClick={this.toggleModal}> Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                    <div className="form-group">
+                        <Label htmlFor="rating" md={10}>Rating</Label>
+                        <Control.select model=".rating " id="rating" name="rating"
+                            className="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                    </div>
+                    <div className="form-group">
+                        <Label htmlFor="author" md={10}>Your Name</Label>
+                        <Control.text model=".author" id="author" name="author"
+                            placeholder="Your Name"
+                            className="form-control"
+                            validators={{
+                                minLength: minLength(2),
+                                maxLength: maxLength(15)
+                            }}
+                        />
+                        <Errors
+                            className="text-danger"
+                            model=".author"
+                            show="touched"
+                            component="div"
+                            messages={{
+                                minLength: 'Must be at least 2 characters',
+                                maxLength: 'Must be 15 characters or less'
+                            }}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <Label htmlFor="comment" md={10}>Comment</Label>
+                        <Control.textarea rows="6" model=".comment" id="comment" name="comment"
+                            placeholder=""
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <Button type="submit" value="submit" color="primary">Submit</Button>
+                    </div>
+                </LocalForm>
+            </ModalBody>
+        </Modal>
+    </>
     );
   }
+}
 
-function RenderComments({comments}) {
-    console.log(comments);
-    if (comments) {
-      // comments !== null
+function RenderCampsite({ campsite }) {
+  return (
+      <div className="col-md-5 m-1">
+          <Card>
+              <CardImg top src={campsite.image} alt={campsite.name} />
+              <CardBody>
+                  <CardText>{campsite.description}</CardText>
+              </CardBody>
+          </Card>
+      </div>
+  );
+}
+
+
+function RenderComments({ comments }) {
+  if (comments) {
       return (
-        <div className="col-md-5 m-1">
-          <h4>Comments</h4>
-          {comments.map((comment) => {
-            return (
-              <div key={comment.id}>
-                <p>{comment.text}</p>
-                -- {comment.author}{" "}
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "short",
-                  day: "2-digit",
-                }).format(new Date(Date.parse(comment.date)))}
-              </div>
-            );
-          })}
-        </div>
-      );
-    } else {
-      return <div>No comments available</div>;
-    }
-  }
+          <>
+              <div className="col-md-5 m-1" >
+                  <h4>Comments</h4>
+                  {comments.map(c => {
+                      return (
+                          <div key={c.id}>
+                              <p>
+                                  {c.text}
+                                  <br />
+                                  <i>{c.author}</i> -- {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(c.date)))}
+                              </p>
+                          </div>
+                        );
+                    })
 
-  //   let arr = ["comment1", "c2", "c3"]
-  //   let arr = ["<div><p>c1</p> --author date </div>", "<div><p>c2</p> --author date </div>", "<div><p>c3</p> --author date </div>"]
-
-function CampsiteInfo(props) {
-      if (props.campsite) {
-          return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <h2>{props.campsite.name}</h2>
-                        <hr />
+                    }
+                    <CommentForm />
+              
                     </div>
-                </div>
-                <div className="row">
-                    <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-        );
-      }
-      return <div />;
-  }
+            </>);
+    }
+    return (<div></div>)
+}
 
+  function CampsiteInfo(props) {
+    if (props.campsite) {
+        return (
+          <div className="container">
+              <div className="row">
+                  <div className="col">
+                      <Breadcrumb>
+                          <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                          <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                      </Breadcrumb>
+                      <h2>{props.campsite.name}</h2>
+                      <hr />
+                  </div>
+              </div>
+              <div className="row">
+                  <RenderCampsite campsite={props.campsite} />
+                  <RenderComments comments={props.comments} />
+              </div>
+          </div>
+      );
+    }
+    return <div />;
+}
 
 export default CampsiteInfo;
+
