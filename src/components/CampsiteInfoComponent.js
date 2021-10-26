7b7f4ac97a58cbe9d6735532f0e6f415753088de
Wen Ow <ow.wen2022@gmail.com>
2021-10-26 17:35:27 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -27,9 +27,8 @@ toggleModal() {
 }
 
 handleSubmit(values) {
-  console.log('Current state is: ' + JSON.stringify(values));
-  alert('Current state is: ' + JSON.stringify(values));
-  this.toggleModal();
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
 }
 
 render() {
@@ -103,7 +102,7 @@ function RenderCampsite({ campsite }) {
 }
 
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, campsiteId}) {
   if (comments) {
       return (
           <>
@@ -122,7 +121,7 @@ function RenderComments({ comments }) {
                     })
 
                     }
-                    <CommentForm />
+                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
               
                     </div>
             </>);
@@ -146,7 +145,11 @@ function RenderComments({ comments }) {
               </div>
               <div className="row">
                   <RenderCampsite campsite={props.campsite} />
-                  <RenderComments comments={props.comments} />
+                  <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
               </div>
           </div>
       );
